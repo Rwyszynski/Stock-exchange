05a1f7a930bb9cb21f5051f440dd2b9a1cf6066b
Robert Wyszynski <robertointerwento@gmail.com>
2024-06-28 09:33:37 +0200

    add graph

--- company/views.py
+++ company/views.py
@@ -5,12 +5,38 @@ import json
 from django.contrib.auth.middleware import AuthenticationMiddleware
 from django.http import JsonResponse
 import requests
+import pandas as pd
+import wget
+import matplotlib
 
 
 def show(request):
 
     response = requests.get(
-        'https://api.twelvedata.com/stocks?country=pl').json()
+        'https://financialmodelingprep.com/api/v3/symbol/WSE?apikey=1naOu5k2WMCBylHjrlsRplGrDXNdSsqt').json()
 
     return render(request, 'index.html', {'response': response})
     # return JsonResponse({'url': url})
+
+
+def diagram(request):
+    # nazwa tickera papieru wartościowego (CDR = CD Projekt SA)
+    TICKER = "CDR"
+    # data początkowa
+    START_DATE = "2024-05-24"
+    # data końcowa
+    END_DATE = "2024-06-24"
+    # plik w którym będą przechowywane archiwalne notowania dla danego tickera
+    FILENAME = "data.txt"
+
+    url = "https://stooq.pl/q/d/l/?s={0}&d1={1}&d2={2}&i=d".format(
+        TICKER, START_DATE.replace("-", ""), END_DATE.replace("-", ""))
+    wget.download(url, FILENAME)
+
+    data_frame = pd.read_csv(FILENAME, index_col='Data',
+                             parse_dates=True, usecols=['Data', 'Zamkniecie'],
+                             na_values='nan')
+    # rename the column header with ticker
+    data_frame = data_frame.rename(columns={'Zamkniecie': TICKER})
+    data_frame.dropna(inplace=True)
+    print(data_frame)
--- testy.py
+++ testy.py
@@ -1,8 +1,46 @@
-import requests
+import pandas as pd
+import wget
+import matplotlib
 
-url = "https://stooq.pl/q/d/l/?s=cdr&d1=20240526&d2=20240626&i=d"
+
+# nazwa tickera papieru wartościowego (CDR = CD Projekt SA)
+TICKER = "CDR"
+# data początkowa
+START_DATE = "2024-05-24"
+# data końcowa
+END_DATE = "2024-06-24"
+# plik w którym będą przechowywane archiwalne notowania dla danego tickera
+FILENAME = "data.txt"
+
+url = "https://stooq.pl/q/d/l/?s={0}&d1={1}&d2={2}&i=d".format(
+    TICKER, START_DATE.replace("-", ""), END_DATE.replace("-", ""))
+wget.download(url, FILENAME)
+
+data_frame = pd.read_csv(FILENAME, index_col='Data',
+                         parse_dates=True, usecols=['Data', 'Zamkniecie'],
+                         na_values='nan')
+# rename the column header with ticker
+data_frame = data_frame.rename(columns={'Zamkniecie': TICKER})
+data_frame.dropna(inplace=True)
+print(data_frame)
+
+# # calculate the standard deviation
+# std_dev = data_frame.rolling(window=20).std()
+# # calculate Simple Moving Average with 20 days window
+# sma = data_frame.rolling(window=20).mean()
+
+# lower_band = sma - 2*std_dev
+# lower_band = lower_band.rename(columns={TICKER: "lower band"})
 
 
-response = requests.get(url)
+# upper_band = sma + 2*std_dev
+# upper_band = upper_band.rename(columns={TICKER: "upper band"})
 
-print(response.json())
+# data_frame = data_frame.join(upper_band).join(lower_band)
+# ax = data_frame.plot(title=TICKER)
+# ax.fill_between(data_frame.index, lower_band['lower band'],
+#                 upper_band['upper band'], color='#5F9F9F')
+# ax.set_xlabel('Date')
+# ax.set_ylabel('Price')
+# ax.grid()
+# plt.show(block=True)
